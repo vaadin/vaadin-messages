78f00649a60a3cae5fb8e512582b1126a7b26273
Jens Jansson <peppe@vaadin.com>
2021-02-01 17:09:48 +0200

    Implement <vaadin-message>

--- theme/lumo/vaadin-message-styles.js
+++ theme/lumo/vaadin-message-styles.js
@@ -8,14 +8,41 @@ registerStyles(
   'vaadin-message',
   css`
     :host {
-      height: calc(var(--lumo-size-l) / 10);
-      margin: var(--lumo-space-s) 0;
+      padding: var(--lumo-space-s);
     }
 
-    [part='value'] {
-      border-radius: var(--lumo-border-radius);
-      background-color: var(--lumo-primary-color);
-      color: white;
+    :host {
+      font-family: var(--lumo-font-family);
+      font-size: var(--lumo-font-size-m);
+    }
+
+    .vaadin-message-wrapper {
+      padding-inline-start: var(--lumo-space-s);
+    }
+
+    /* IE11 is the only browser that we support that doesn't support
+     * padding-inline-start. For IE11, we use padding-left and -right instead. */
+    @media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
+      .vaadin-message-wrapper {
+        padding-left: var(--lumo-space-s);
+        padding-right: 0;
+      }
+      :host([dir='rtl']) .vaadin-message-wrapper {
+        padding-left: 0;
+        padding-right: var(--lumo-space-s);
+      }
+    }
+
+    .vaadin-message-header {
+      padding-bottom: var(--lumo-space-s);
+    }
+
+    [part='name'] {
+      font-weight: 500;
+    }
+
+    [part='time'] {
+      color: var(--lumo-secondary-text-color);
     }
   `,
   { moduleId: 'lumo-message' }
--- theme/material/vaadin-message-styles.js
+++ theme/material/vaadin-message-styles.js
@@ -1,21 +1,46 @@
 import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import '@vaadin/vaadin-material-styles/color.js';
+import '@vaadin/vaadin-material-styles/typography.js';
 
 registerStyles(
   'vaadin-message',
   css`
     :host {
-      margin: 8px 0;
-      height: 32px;
+      padding: 8px;
     }
 
-    [part='value'] {
-      color: black;
+    :host {
+      font-family: var(--material-font-family);
+      font-size: var(--material-small-font-size);
     }
 
-    /* RTL specific styles */
-    :host([dir='rtl']) [part='value'] {
-      transform-origin: 100% 0;
+    .vaadin-message-wrapper {
+      padding-inline-start: 8px;
+    }
+
+    .vaadin-message-header {
+      padding-bottom: 8px;
+    }
+
+    [part='name'] {
+      font-weight: 500;
+    }
+
+    [part='time'] {
+      color: var(--material-secondary-text-color);
+    }
+
+    /* IE11 is the only browser that we support that doesn't support
+     * padding-inline-start. For IE11, we use padding-left and padding-right instead. */
+    @media all and (-ms-high-contrast: active), (-ms-high-contrast: none) {
+      .vaadin-message-wrapper {
+        padding-left: 8px;
+        padding-right: 0;
+      }
+      :host([dir='rtl']) .vaadin-message-wrapper {
+        padding-left: 0;
+        padding-right: 8px;
+      }
     }
   `,
   { moduleId: 'material-message' }
