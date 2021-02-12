06198dcc63284021660ea7419a921370b256fee1
Jens Jansson <peppe@vaadin.com>
2021-02-12 12:33:45 +0200

    Add documentation to message-input

--- src/vaadin-message-input.d.ts
+++ src/vaadin-message-input.d.ts
@@ -3,7 +3,25 @@ import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mix
 import { ElementMixin } from '@vaadin/vaadin-element-mixin/vaadin-element-mixin.js';
 
 /**
- * `<vaadin-message-input>` is a Web Component ...
+ * `<vaadin-message-input>` is a Web Component for sending messages.
+ * It consists of a text area that grows on along with the content, and a send button to send message.
+ *
+ * ```html
+ * <vaadin-message-input></vaadin-message-input>
+ * ```
+ *
+ * ### Styling
+ *
+ * The following shadow DOM parts are available for styling:
+ *
+ * Part name | Description
+ * ----------|----------------
+ *
+ * See [ThemableMixin – how to apply styles for shadow parts](https://github.com/vaadin/vaadin-themable-mixin/wiki)
+ *
+ * @extends HTMLElement
+ * @mixes ThemableMixin
+ * @mixes ElementMixin
  */
 declare class MessageInputElement extends ThemableMixin(ElementMixin(HTMLElement)) {}
 
--- src/vaadin-message-input.js
+++ src/vaadin-message-input.js
@@ -9,7 +9,25 @@ import { ElementMixin } from '@vaadin/vaadin-element-mixin/vaadin-element-mixin.
 import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@vaadin/vaadin-text-field/src/vaadin-text-area.js';
 /**
- * `<vaadin-message-input>` is a Web Component ...
+ * `<vaadin-message-input>` is a Web Component for sending messages.
+ * It consists of a text area that grows on along with the content, and a send button to send message.
+ *
+ * ```html
+ * <vaadin-message-input></vaadin-message-input>
+ * ```
+ *
+ * ### Styling
+ *
+ * The following shadow DOM parts are available for styling:
+ *
+ * Part name | Description
+ * ----------|----------------
+ *
+ * See [ThemableMixin – how to apply styles for shadow parts](https://github.com/vaadin/vaadin-themable-mixin/wiki)
+ *
+ * @extends HTMLElement
+ * @mixes ThemableMixin
+ * @mixes ElementMixin
  */
 class MessageInputElement extends ElementMixin(ThemableMixin(PolymerElement)) {
   static get template() {
