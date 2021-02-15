99506191290184ff08e76ef41ac4c03ad1ea5ce3
Jens Jansson <peppe@vaadin.com>
2021-02-15 16:01:00 +0200

    Add text-field styles
    These didn't come automatically via text-area anymore after downgrade from 3.0 to 2.8

--- theme/lumo/vaadin-message-input-styles.js
+++ theme/lumo/vaadin-message-input-styles.js
@@ -5,6 +5,7 @@ import '@vaadin/vaadin-lumo-styles/spacing.js';
 import '@vaadin/vaadin-lumo-styles/style.js';
 import '@vaadin/vaadin-button/theme/lumo/vaadin-button-styles.js';
 import '@vaadin/vaadin-text-field/theme/lumo/vaadin-text-area-styles.js';
+import '@vaadin/vaadin-text-field/theme/lumo/vaadin-text-field-styles.js';
 
 registerStyles(
   'vaadin-message-input',
