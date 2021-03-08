128b94ca9085009e1e1ed37d79776a13b8e1f28d
Jens Jansson <peppe@vaadin.com>
2021-03-08 13:53:19 +0200

    Fix style references

--- theme/lumo/vaadin-message-input-styles.js
+++ theme/lumo/vaadin-message-input-styles.js
@@ -3,6 +3,7 @@ import '@vaadin/vaadin-lumo-styles/color.js';
 import '@vaadin/vaadin-lumo-styles/sizing.js';
 import '@vaadin/vaadin-lumo-styles/spacing.js';
 import '@vaadin/vaadin-lumo-styles/style.js';
+import './vaadin-message-input-text-area-styles.js';
 import '@vaadin/vaadin-button/theme/lumo/vaadin-button-styles.js';
 
 registerStyles(
