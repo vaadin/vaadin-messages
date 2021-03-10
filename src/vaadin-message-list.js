a9e036ac90368caafda799d042377e9b5a5b844d
Jens Jansson <Peppe@users.noreply.github.com>
2021-03-10 16:45:50 +0200

    Update src/vaadin-message-list.js
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>

--- src/vaadin-message-list.js
+++ src/vaadin-message-list.js
@@ -181,7 +181,7 @@ class MessageListElement extends ElementMixin(ThemableMixin(PolymerElement)) {
 
   _handleFocusEvent(e) {
     this._setTabIndexesByMessage(
-      e.composedPath().filter(elem => elem.tagName === 'VAADIN-MESSAGE')[0]);
+      e.composedPath().filter((elem) => elem.tagName.toLowerCase() === 'vaadin-message')[0]);
   }
   /**
    * @param {number} idx
